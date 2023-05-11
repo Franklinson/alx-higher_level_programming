6633ae3ae20cfb53597d913f3c9879967fe1463a
Franklinson <franklinhassey@gmail.com>
2023-05-10 18:34:59 -0700

    isupper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if "a" <= char <= "z" else char
+        uppercase_str += uppercase_char
+
+    print(uppercase_str, end="\n")
