188ee91531a6aaa1b3ec746f4c48ca8576de9d26
Franklinson <franklinhassey@gmail.com>
2023-05-10 17:34:42 -0700

    ascii

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-output = ""
 
 for ascii_code in range(ord('a'), ord('z') + 1):
-    output += "{}".format(chr(ascii_code))
-
-print(output)
-
+    print("{:s}".format(chr(ascii_code)), end='')
