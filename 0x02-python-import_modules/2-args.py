107200dc2c2d0feaf61ddc528d71411c8eff4efb
Franklinson <franklinhassey@gmail.com>
2023-05-13 09:12:52 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def print_arguments(argv):
+    num_arguments = len(argv)
+
+    if num_arguments == 0:
+        print("0 arguments.")
+        print(".")
+    else:
+        plural = "s" if num_arguments > 1 else ""
+        print(f"{num_arguments} argument{plural}:")
+
+        for i, arg in enumerate(argv):
+            print(f"{i+1}: {arg}")
+
+    print()
+
+
+if __name__ == "__main__":
+
+    print_arguments(sys.argv[1:])
