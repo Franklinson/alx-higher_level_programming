d6b68cc9f13e163a56fde7be9c1c46c44027ee98
Franklinson <franklinhassey@gmail.com>
2023-05-13 08:50:55 -0700

    calc import

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} is {}".format(a, b, mul(a, b)))
+print("{} / {} is {}".format(a, b, div(a, b)))
+
