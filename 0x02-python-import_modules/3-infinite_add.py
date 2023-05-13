905200b9e0914fbc3cfd4f2df69b94f67a6d33a5
Franklinson <franklinhassey@gmail.com>
2023-05-13 09:23:05 -0700

    args addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def add_arguments(*args):
+    result = sum(int(arg) for arg in args)
+    print(result)
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    add_arguments(*arguments)
