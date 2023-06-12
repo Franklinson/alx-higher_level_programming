f7de06904ec2d9c8555684f22c502138700145b5
Franklinson <franklinhassey@gmail.com>
2023-06-12 16:12:21 -0700

    look up

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj: An object to inspect.
+
+    Returns:
+        A list of strings available attributes and methods of the object.
+    """
+    attributes = []
+    methods = []
+
+    for attr in dir(obj):
+        if not attr.startswith("__"):
+            value = getattr(obj, attr)
+            if callable(value):
+                methods.append(attr)
+            else:
+                attributes.append(attr)
+
+    return attributes + methods
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Python Inheritance with Alx
