4ebedca93835d4a15784ae5796c502d5cc8f2d89
Franklinson <franklinhassey@gmail.com>
2023-06-15 00:18:16 -0700

    write a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,6 +12,6 @@ def write_file(filename="", text=""):
 
     Returns:
         int: The number of characters written to the file.
-        """
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return f.write(text)
