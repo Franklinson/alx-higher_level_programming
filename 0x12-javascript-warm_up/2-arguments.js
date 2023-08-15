201a30f4050f60f18ffdd36e3bede6da47b60400
Franklinson <franklinhassey@gmail.com>
2023-08-15 02:11:15 +0000

    argument for prints

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const myArg = process.argv.length - 2;
 if (myArg === 0) {
   console.log('No argument');
 } else if (myArg === 1) {
- console.log('Argument found');
+  console.log('Argument found');
 } else {
- console.log('Arguments found');
+  console.log('Arguments found');
 }
