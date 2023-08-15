604757dd2f0e4fc3b8ca59b13efb6ad756114132
Franklinson <franklinhassey@gmail.com>
2023-08-15 02:50:22 +0000

    converting to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArg = process.argv[2];
+
+const parsedInt = parseInt(myArg);
+
+if (!isNaN(parsedInt)) {
+  console.log(`My number: ${parsedInt}`);
+} else {
+  console.log("Not a number");
+}
