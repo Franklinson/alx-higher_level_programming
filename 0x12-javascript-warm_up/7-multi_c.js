d1d468d0d21920fb7c5a2d41ff5096b656d9dad7
Franklinson <franklinhassey@gmail.com>
2023-08-15 15:55:28 +0000

    intro to js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
+const x = process.argv[2];
 
-const myArgs = process.argv.slice(2);
-
-if (myArgs.length === 0) {
+if (!parseInt(x)) {
   console.log('Missing number of occurrences');
 } else {
-  try {
-    const occurrences = Number(myArgs[0]);
-
-    for (let i = 0; i < occurrences; i++) {
-      console.log('C is fun');
-    }
-  } catch (error) {
-    console.log('Missing number of occurrences');
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
