e33887fe6011d71bb9b7cf588a7a515d1e302f4d
Franklinson <franklinhassey@gmail.com>
2023-08-24 06:39:31 -0700

    model of a state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-An sqlalchemy script based on State class and a base
+This script defines a State class
 """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
+
 class State(Base):
-     """the state class links to the state table"""
+    """State class
+    """
+    __tablename__ = 'states'
 
-    __tablename__ = 'state'
-
-    id = Column(Integer, autoincreament=True primary_key=True, nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
