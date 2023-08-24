3e5802b7e0bde11726c835038a775ef2a9d432ae
Franklinson <franklinhassey@gmail.com>
2023-08-23 17:00:01 -0700

    get state from commandline arg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,21 +2,22 @@
 """
 This takes an argument in the command line and displays the values in the state that match the argument from the database
 """
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
 
-    usr = argv[1]
-    usr_pass = argv[2]
-    db_name = argv[3]
+    #usr = argv[1]
+    #usr_pass = argv[2]
+    #db_name = argv[3]
 
     conn = MySQLdb.connect(
             host-"localhost",
             port=3306,
-            user=usr,
-            passwd=usr_pass,
-            db=db_name
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
             )
     cur = conn.cursor()
     cur.execute("SELECT * FROM staes WHERE (name) LIKE BINARY '{}'\
