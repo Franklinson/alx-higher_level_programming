9e092af49628e959303702c154580747cd3356ce
Franklinson <franklinhassey@gmail.com>
2023-08-24 09:12:43 -0700

    delete state starting with letter a in sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Delete all state that start with the letter a.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.contains('a'))
+    if states is not None:
+        for state in states:
+            session.delete(state)
+
+    session.commit()
+
+    session.close()
