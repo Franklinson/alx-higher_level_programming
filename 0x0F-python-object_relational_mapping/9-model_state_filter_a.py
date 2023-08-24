6c06aca494191e3a423a8d2d62c2426daf0c8740
Franklinson <franklinhassey@gmail.com>
2023-08-24 07:57:54 -0700

    letters of a in a state

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(engine)
     session = Session()
-    states = session.query(State).filter(State.name.ilike('%a%'))\
-                                 .order_by(State.id)
+    states = session.query(State).filter(State.name.contains('a'))
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
