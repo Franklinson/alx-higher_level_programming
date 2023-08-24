d8fc9334a5376245c5af1e3007a7a6eca836f311
Franklinson <franklinhassey@gmail.com>
2023-08-24 09:57:01 -0700

    quick bug fixed

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-'''fetching data from the city and state class'''
+"""
+This is the city class
+"""
 
-
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from sys import argv
+from model_state import State, Base
 from model_city import City
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))
+
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    state_and_city = session.query(City, State).order_by(City.id).\
-        join(State, City.state_id == State.id).all()
-    for city, state in state_and_city:
-        print("{}:{} {}".format(state.name, city.id, city.name)
+
+    results = session.query(City, State).join(State)
+
+    for city, state in results.all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.commit()
     session.close()
