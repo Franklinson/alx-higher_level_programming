f21bf93a7a346797a4dd13c8bd6cc098dbd0dde4
Franklinson <franklinhassey@gmail.com>
2023-08-23 17:23:00 -0700

    prevent sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     db_cursor.execute(
         "SELECT * FROM states WHERE %(name) LIKE BINARY ORDER BY \
-                states.id ASC".{'name':argv[4]})
+                states.id ASC",{'name':argv[4]})
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
