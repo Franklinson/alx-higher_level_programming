9921f6e7520cab0e86b1ab21962a0282166ff582
Franklinson <franklinhassey@gmail.com>
2023-08-24 09:49:13 -0700

    fetch data from state and city classes

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-This script defines a City class
+City class
 """
-
-from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
     """City class
     """
+
     __tablename__ = 'cities'
 
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
