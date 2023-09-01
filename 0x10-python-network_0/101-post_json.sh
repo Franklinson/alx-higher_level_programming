ab3cf12b32c838f7673906814abbf6f3c86fdbc5
Franklinson <franklinhassey@gmail.com>
2023-08-31 19:15:13 -0700

    equest to 0.0.0.0:5000/catch_me

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send the JSON POST request using curl and display the response body
-curl -s -X POST -H "Content-Type: application/json" -d "$2" "$1"
+curl -s -H "Content-Type: application/json" -d "$2" "$1"
---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# # Send the request using curl and capture the response in a variable
+curl -X GET 0.0.0.0:5000/catch_me
+
