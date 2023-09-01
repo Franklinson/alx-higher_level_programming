6968dd37f30a6d18dd2f6de9771efbbc77620af5
Franklinson <franklinhassey@gmail.com>
2023-09-01 22:34:03 +0000

    sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to ..."""
+
+import requests
+import sys
+
+
+def search_user(letter):
+    url = "http://0.0.0.0:5000/search_user"
+    params = {"q": letter}
+
+    response = requests.post(url, params=params)
+
+    try:
+        data = response.json()
+        if data:
+            print(f"[{data['id']}] {data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    search_user(letter)
