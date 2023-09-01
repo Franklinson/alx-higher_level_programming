7d6e3b23de684957a3be2494ce630e6870c3d698
Franklinson <franklinhassey@gmail.com>
2023-09-01 22:23:10 +0000

    get error code from command line

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,23 +5,6 @@ import requests
 import sys
 
 
-def send_post_request(url, email):
-    response = requests.post(url, data={'email': email})
-    return response.content
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <URL> <email>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    response = send_post_request(url, email)
-
-    if response.status_code == 200:
-        print("POST request successful. Response content:")
-        print(response.content.decode('utf-8'))
-    else:
-        print(f"POST request failed with status code {response.status_code}")
+    res = requests.post(sys.argv[1], data={"email": sys.argv[2]})
+    print(res.text)
