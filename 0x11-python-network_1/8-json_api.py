bb3ef03392774115fa1b89b8046ea7bfbdfa7002
Franklinson <franklinhassey@gmail.com>
2023-09-01 23:01:56 +0000

    made corrections

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,13 +9,16 @@ def search_user(letter):
     url = "http://0.0.0.0:5000/search_user"
     params = {"q": letter}
 
-    response = requests.post(url, params=params)
+    try:
+        response = requests.post(url, params=params)
 
         data = response.json()
         if data:
             print(f"[{data['id']}] {data['name']}")
         else:
             print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
 
 if __name__ == "__main__":
