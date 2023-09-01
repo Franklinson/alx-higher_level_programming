92b77e8a1572cdc316fab4979f79ba9ad79e6255
Franklinson <franklinhassey@gmail.com>
2023-09-01 21:46:16 +0000

    send request to URL

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request"""
+
+import sys
+import urllib.request
+import urllib.error
+
+def fetch_url_content(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            status_code = response.getcode()
+            content = response.read().decode('utf-8')
+            return status_code, content
+    except urllib.error.HTTPError as e:
+        return e.code, None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    status_code, content = fetch_url_content(url)
+
+    if content is not None:
+        print(f"HTTP Status Code: {status_code}")
+        print(content)
+    else:
+        print(f"Error code: {status_code}")
