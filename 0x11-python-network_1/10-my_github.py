2151d0cf1b53e9c03c7b49e16881e97dc3ef1d44
Franklinson <franklinhassey@gmail.com>
2023-09-01 22:51:07 +0000

    github details

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """takes your GitHub credentials (username and password)"""
-import requests
+
 import sys
-
-
-def get_github_id(username, password):
-    """Gets the GitHub ID of the user with the given username and password."""
-    url = f"https://api.github.com/users/{username}"
-    headers = {"Authorization": f"Basic {password}"}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()["id"]
-    else:
-        raise Exception(f"Error getting GitHub ID: {response.status_code}")
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    github_id = get_github_id(username, password)
-    print(f"{github_id}")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
