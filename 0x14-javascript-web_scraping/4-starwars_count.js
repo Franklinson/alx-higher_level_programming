ae7f75d4f2d9a779b5fc317ea262fcb16ed10b05
Franklinson <franklinhassey@gmail.com>
2023-09-27 00:08:29 +0000

    find a title

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,24 @@
 
 const request = require('request');
 
-const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
-const WEDGE_ANTILLES_ID = 18;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const characterId = 18;
 
-const urlWithFilter = `${API_URL}?characters=${WEDGE_ANTILLES_ID}`;
-
-request.get(urlWithFilter, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
 
+  if (response.statusCode !== 200) {
+    console.error(`API request failed with status code ${response.statusCode}`);
+    return;
+  }
+
   const filmsData = JSON.parse(body);
+  const filmsWithWedgeAntilles = filmsData.results.filter(film =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  );
 
-  const numMovies = filmsData.count;
-
-  console.log(numMovies);
+  console.log(filmsWithWedgeAntilles.length);
 });
