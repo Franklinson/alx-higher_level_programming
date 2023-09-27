0990338cf3557a01d84b23e75ad24c0d4f594ffc
Franklinson <franklinhassey@gmail.com>
2023-09-27 00:22:11 +0000

    content of a webpage to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,14 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
+const argv = process.argv;
+const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.error('Usage: node fetchAndSaveWebpage.js <URL> <outputFilePath>');
-  process.exit(1);
-}
-
-request(process.argv[2])
-  .on('error', (error) => {
-    console.error('Error:', error);
-    process.exit(1);
-  })
-  .pipe(fs.createWriteStream(process.argv[3]))
-  .on('error', (error) => {
-    console.error('Error writing to file:', error);
-    process.exit(1);
-  })
-  .on('finish', () => {
-    console.log(`Webpage content saved to ${process.argv[3]}`);
+const url = argv[2];
+const file = argv[3];
+request(url, function (error, response, body) {
+  if (error) console.error(error);
+  const content = body;
+  fs.writeFile(file, content, (err) => {
+    if (err) console.error(err);
   });
+});
